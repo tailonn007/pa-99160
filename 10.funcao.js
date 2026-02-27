8f8a55e99dccb06321d833410ba54a6e10fe28ef
are <cauanj.r.p@gmail.com>
2026-02-26 21:39:57 -0300

    funcao

---
+++ 10.funcao.js
@@ -0,0 +1,15 @@
+// função
+
+function somar(a, b){
+    return a+b
+}
+
+function sutrair(a, b){
+    const subtração = sutrair(2,3)
+}
+
+const soma= somar(2,3)
+const subtração= subtrair(2,3)
+
+console.log(`soma: ${soma}`)
+console.log(`subtração: ${subtração}`)
