6ccf2f31b8c3e22f615baa8c8db73b6be8550801
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-05 21:34:48 -0300

    atualizei

---
+++ html-css-js/script.js
@@ -0,0 +1,28 @@
+function gerarTabuada(){
+    // pegar o valor do input no html.
+    const numeroinput = document.getElementById("numeroInput")
+    let numero = parseInt(numeroinput.value);
+
+// pegar o elemento html onde a tabuada  será exibida.
+    const resultadoDiv = document.getElementById("resultadoTabuada")
+    resultadoDiv.innerHTML= "";
+
+// verifica se a entrada é um numero válida
+    if (isNaN(numero)  || numero === null || numero === ""){
+        resultadoDiv.innerHTML = "<p> por favor, digite um numero... <\p>";
+    return;
+
+}
+// adicionar titulo  para tabuada.
+    resultadoDiv.innerHTML += `<h2>tabuada do ${numero}<\h2>`;
+
+// laço de repetição para gerar tabuada de 1 até 10.
+    for (let i = i ; i <= 10; i++){
+        let resultado = numero * i; 
+        resultadoDiv.innerHTML += `<p> ${numero} x ${i} = ${resultado}<\p>`
+    }
+}
+
+
+const gerarBotao =  document.getElementById("gerarbotao");
+gerarBotao.addEventListener("click", gerarTabuada);
