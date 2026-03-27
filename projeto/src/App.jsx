ceba3dc2f1c8a99cc202c670acc0e337a7c1d562
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-26 21:22:12 -0300

    revisão

---
+++ projeto/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Mensagem from './components/mensagem'
+
+function App() {
+
+  return (
+    <>
+      <h1>bem-vindo ao meu projeto!</h1>
+      <Mensagem />
+      <Mensagem />
+      <Mensagem />
+      <Mensagem />
+      <Mensagem />
+    </>
+  )
+}
+
+export default App
---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+export default function Mensagem() {
+    return(
+        <div className='mensagem'>
+            <h1>componente</h1>
+            <p>este é um componente simples</p>
+        </div>
+    )
+    
+}
